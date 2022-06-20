01068bc36d9527484efa3c2f6d8782a5e6c62abf
littlelib <littlelib@naver.com>
2022-06-20 23:45:25 +0900

    create admin page

--- admin/content.js
+++ admin/content.js
@@ -1,7 +1,8 @@
 async function getNumCand() {
-	let a=await fetch("https://docs.google.com/spreadsheets/d/1WXbi1DWnh16TTR7fabfIfbBIDUewg5DBS6IS6B5ukYE/gviz/tq?tqx=out:csv&sheet=EMR_dict")
+	let a=await fetch("https://docs.google.com/spreadsheets/d/1WXbi1DWnh16TTR7fabfIfbBIDUewg5DBS6IS6B5ukYE/gviz/tq?tqx=out:csv&sheet=candidates")
 	.then(res=>res.text());
 	let temp=a.split("\"\n");
+	console.log(temp.length);
 	document.getElementById("numCand").value=temp.length-1;
 }
 
