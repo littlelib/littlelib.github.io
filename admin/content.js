e0d469886c66796dc631fe3078e1d4441c5881f7
shw <littlelib>
2022-06-22 16:57:44 +0900

    add minimal logging(access date)

--- admin/content.js
+++ admin/content.js
@@ -6,5 +6,33 @@ async function getNumCand() {
 	document.getElementById("numCand").innerText=temp.length-1;
 }
 
-getNumCand();
+function parseLog(log) {
+	let temp=log.split("\n");
+	let returnArr=[];
+	for(i=0;i<temp.length;i++){
+		returnArr.push(temp[i].split(" "));
+	}
+	return returnArr;
+}
 
+
+async function getMinimalLog() {
+	let a=await fetch("https://docs.google.com/spreadsheets/d/14N-clUHjtCUYpLOIu51C1_JwFrtXdLgl1CPffPuLrY0/gviz/tq?tqx=out:csv")
+	.then(res=>res.text());
+	let logs=parseLog(a);
+	let total=logs.length;
+	let today=0;
+	let now=Date.split(" ").slice(0,4);
+	for(i=0;i<logs.length;i++){
+		if(now==logs[i].slice(0,4)){
+			today++;
+		}
+	}
+	document.getElementById("numVisitTotal").innerText=total;
+	document.getElementById("numVisitToday").innerText=today;
+}
+
+
+
+getNumCand();
+getMinimalLog();
